4c539091dab67097390cc3da51efc599420b395d
Peaklatice-9348 <yusufjavaid.072012@gmail.com>
2025-02-27 19:56:21 +0000

    Add files via upload

--- flappy bird/main.py
+++ flappy bird/main.py
@@ -1,3 +1,4 @@
+#remember collision of pipes if pygame.sprite.groupcollide(bird_group, pipe_group, False, False)
 import pygame
 from pygame.locals import *
 from random import randint
@@ -10,6 +11,9 @@ HEIGHT = 936
 
 #variables
 game_over = False
+pipe_gap = 150
+pipe_frequency = 1500
+last_pipe = pygame.time.get_ticks()-pipe_frequency
 flying = False
 ground_scroll = 0
 scroll_speed = 5
@@ -27,7 +31,7 @@ class Chicken(pygame.sprite.Sprite):
         self.index = 0
         self.v = 0
         self.clicked = False
-        self.max_v = 15
+        self.max_v = 20
         self.counter = 0
         for num in range (1,5):
             img = pygame.image.load(f'flappy bird/images/bird{num}.png')
@@ -44,11 +48,11 @@ class Chicken(pygame.sprite.Sprite):
             if self.rect.bottom < HEIGHT - 168:
                 self.rect.y += int(self.v)
         if game_over == False:
-            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-                self.clicked == True
-                self.v = -10
-            if pygame.mouse.get_pressed()[0] == 0:
-                self.clicked == False
+            if pygame.mouse.get_pressed()[0] != 1:
+                self.clicked == False            
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False and self.rect.top > 0:
+                    self.clicked == True
+                    self.v = -7
             self.counter += 1
             flap = 10
             if self.counter > flap:
@@ -63,11 +67,28 @@ class Chicken(pygame.sprite.Sprite):
             flying = False
             self.v = 0
 
+class Drains(pygame.sprite.Sprite):
+    def __init__(self,x,y,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('flappy bird/images/pipe.png')
+        self.rect = self.image.get_rect()
+        if pos == 1:
+            self.image = pygame.transform.flip(self.image,False,True)
+            self.rect.bottomleft = [x,y - pipe_gap/2]
+        if pos == -1:
+             self.rect.topleft = [x,y + pipe_gap/2]           
+    def update(self):
+        self.rect.x -= 5 
+        if self.rect.right < 0:
+            self.kill()
+        
+
+        
          
 chicken = Chicken(100,int(HEIGHT/2))
 chicken_group = pygame.sprite.Group()
 chicken_group.add(chicken)
-
+drainss = pygame.sprite.Group()
 
 
 
@@ -79,7 +100,18 @@ while running:
     screen.blit(backround,(0,0))
     chicken_group.draw(screen)
     chicken_group.update()
+    drainss.draw(screen)
     screen.blit(ground,(ground_scroll,HEIGHT - 168))
+
+    time = pygame.time.get_ticks()
+    if time - last_pipe > pipe_frequency:
+        pipe_height = randint(-150,150)
+        top_pipe = Drains(WIDTH,int(HEIGHT/2) + pipe_height,1)
+        bottom_pipe = Drains(WIDTH,int(HEIGHT/2) + pipe_height,-1)
+        drainss.add(top_pipe)
+        drainss.add(bottom_pipe)
+        last_pipe = time
+    drainss.update()
     if game_over == False:
         ground_scroll -= scroll_speed
         if abs(ground_scroll) > 35:
