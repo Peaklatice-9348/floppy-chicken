301b5f2e757deeb70f3986466ac5bd8ea536909c
Peaklatice-9348 <yusufjavaid.072012@gmail.com>
2025-03-06 19:59:00 +0000

    Add files via upload

--- flappy bird/main.py
+++ flappy bird/main.py
@@ -10,19 +10,33 @@ WIDTH = 864
 HEIGHT = 936
 
 #variables
+highscore = 10
 game_over = False
-pipe_gap = 150
-pipe_frequency = 1500
+pipe_gap = 200
+pipe_frequency = 1000
 last_pipe = pygame.time.get_ticks()-pipe_frequency
 flying = False
 ground_scroll = 0
 scroll_speed = 5
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-font = pygame.font.SysFont('impact',200)
+font = pygame.font.SysFont('impact',50)
+score = 0
 CAPTION = pygame.display.set_caption('Floppy Chicken')
 
 backround = pygame.image.load('flappy bird/images/flappy bird bg.png')
 ground = pygame.image.load('flappy bird/images/flappy bird ground.png')
+
+def draw_text(text,font,color,x,y):
+    ing = font.render(text,True,color)
+    screen.blit(ing,(x,y))
+
+def reset_game():
+    chicken.v = 0
+    chicken.rect.y = HEIGHT/2
+    drainss.empty()
+    score = 0
+    game_over = False
+    flying = False
 
 class Chicken(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -39,6 +53,7 @@ class Chicken(pygame.sprite.Sprite):
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = [x,y]
+
     def update(self):
         global game_over, flying
         if flying == True:
@@ -47,12 +62,14 @@ class Chicken(pygame.sprite.Sprite):
                 self.v = self.max_v
             if self.rect.bottom < HEIGHT - 168:
                 self.rect.y += int(self.v)
+
         if game_over == False:
             if pygame.mouse.get_pressed()[0] != 1:
                 self.clicked == False            
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False and self.rect.top > 0:
                     self.clicked == True
                     self.v = -7
+
             self.counter += 1
             flap = 10
             if self.counter > flap:
@@ -61,37 +78,49 @@ class Chicken(pygame.sprite.Sprite):
                 if self.index >= len(self.images):
                     self.index = 0
             self.image = self.images[self.index]
+
+        if chicken.rect.bottom > HEIGHT - 168 or pygame.sprite.groupcollide(chicken_group,drainss,False,False):
+            if game_over == False:
+                game_over = True
+                self.v = 5
+
         if chicken.rect.bottom > HEIGHT - 168:
             self.rect.bottom = HEIGHT - 168
-            game_over = True
-            flying = False
-            self.v = 0
 
 class Drains(pygame.sprite.Sprite):
     def __init__(self,x,y,pos):
         pygame.sprite.Sprite.__init__(self)
+        self.passed = False
         self.image = pygame.image.load('flappy bird/images/pipe.png')
         self.rect = self.image.get_rect()
+
         if pos == 1:
             self.image = pygame.transform.flip(self.image,False,True)
             self.rect.bottomleft = [x,y - pipe_gap/2]
+
         if pos == -1:
-             self.rect.topleft = [x,y + pipe_gap/2]           
+             self.rect.topleft = [x,y + pipe_gap/2]   
+
     def update(self):
         self.rect.x -= 5 
         if self.rect.right < 0:
             self.kill()
-        
 
-        
-         
+class Button:
+    global game_over, flying
+    def __init__(self,x,y):
+        self.image = pygame.image.load('flappy bird/images/restart.png')
+        self.x = x
+        self.y = y
+        self.rect = self.image.get_rect()
+
+    def draw(self):
+        screen.blit(self.image,(self.rect.x,self.rect.y))
+       
 chicken = Chicken(100,int(HEIGHT/2))
 chicken_group = pygame.sprite.Group()
 chicken_group.add(chicken)
 drainss = pygame.sprite.Group()
-
-
-
 
 running = True
 while running:
@@ -103,6 +132,13 @@ while running:
     drainss.draw(screen)
     screen.blit(ground,(ground_scroll,HEIGHT - 168))
 
+    if len(drainss)>0:
+        if drainss.sprites()[0].rect.left < chicken_group.sprites()[0].rect.left:
+            if drainss.sprites()[0].passed == False:
+                score += 1
+                drainss.sprites()[0].passed = True
+    draw_text(f'Score{str(score)}',font,'black',50,25)
+    draw_text(f'Highscore:{str(highscore)}',font,'black',50,100)
     time = pygame.time.get_ticks()
     if time - last_pipe > pipe_frequency:
         pipe_height = randint(-150,150)
@@ -111,15 +147,20 @@ while running:
         drainss.add(top_pipe)
         drainss.add(bottom_pipe)
         last_pipe = time
-    drainss.update()
+    if game_over == False:
+        drainss.update()
     if game_over == False:
         ground_scroll -= scroll_speed
+
         if abs(ground_scroll) > 35:
             ground_scroll = 0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
             flying = True
+
     pygame.display.update()
 pygame.quit()
